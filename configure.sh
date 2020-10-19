1e813838729c86de4a1c4b470e32433540e59b57
leigingban <leigingban@aliyun.com>
2020-10-19 14:22:07 +0800

    更改安装目录

--- configure.sh
+++ configure.sh
@@ -7,11 +7,11 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/trojan-go/trojan-go-linux-amd64.zip
 
 unzip /tmp/trojan-go/trojan-go-linux-amd64.zip -d /tmp/trojan-go
 
-mkdir /app/trojan-go
+mkdir /usr/bin/trojan-go
 
-install -m 755 /tmp/trojan-go/trojan-go /app/trojan-go/trojan-go
-install -m 755 /tmp/trojan-go/geoip.dat /app/trojan-go/geoip.dat
-install -m 755 /tmp/trojan-go/geosite.dat /app/trojan-go/geosite.dat
+install -m 755 /tmp/trojan-go/trojan-go /usr/bin/trojan-go/trojan-go
+install -m 755 /tmp/trojan-go/geoip.dat /usr/bin/trojan-go/geoip.dat
+install -m 755 /tmp/trojan-go/geosite.dat /usr/bin/trojan-go/geosite.dat
 
 
 # Remove temporary directory
@@ -19,7 +19,7 @@ rm -rf /tmp/trojan-go
 
 # trojan-go new configuration
 # 注意 \\ 将会被转义 “\\” -> "\"
-cat << EOF > /app/trojan-go/server.json
+cat << EOF > /usr/bin/trojan-go/server.json
 {
     "run_type": "server",
     "local_addr": "0.0.0.0",
@@ -39,12 +39,12 @@ cat << EOF > /app/trojan-go/server.json
         "block": [
             "geoip:private"
         ],
-        "geoip": "/app/trojan-go/geoip.dat",
-        "geosite": "/app/trojan-go/geosite.dat"
+        "geoip": "/usr/bin/trojan-go/geoip.dat",
+        "geosite": "/usr/bin/trojan-go/geosite.dat"
     }
 }
 EOF
 
-cat /app/trojan-go/server.json
+cat /usr/bin/trojan-go/server.json
 # Run trojan-go
-/app/trojan-go/trojan-go -config /app/trojan-go/config.json
+/usr/bin/trojan-go/trojan-go -config /usr/bin/trojan-go/config.json
